91e6c8777b6648a9e940b85e159d3aa9157b14c1
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-02 20:17:02 +0500

    WEBSITE: application prototype is rendered completely

--- src/website/views.py
+++ src/website/views.py
@@ -4,6 +4,11 @@ from django.views.generic import TemplateView, ListView
 class HomeView(TemplateView):
     template_name = 'website/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['home'] = True
+        return context
+
 
 class PrivacyPolicyView(TemplateView):
     template_name = 'website/policy.html'
