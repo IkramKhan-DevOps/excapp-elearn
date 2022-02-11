8496a6b7b5fc5a9833e8603725617c0076c3fa9d
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-11 18:44:55 +0500

    PROJECT: assets removed

--- assets/core/js/back_click.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function (event) {
-    history.pushState(null, null, location.href);
-    window.onpopstate = function () {
-        history.go(1);
-    }
-});
--- assets/core/js/right_click.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function (event) {
-    document.addEventListener('contextmenu', event => event.preventDefault());
-});
